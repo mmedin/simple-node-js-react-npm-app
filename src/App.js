60cb8c584f147951e29c6dcba979cf2eee477cc4
mmedin <miguel.e.medina@gmail.com>
2022-10-17 17:12:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React para los amigos de BBVA de Agosto</h1>
+          <h1 className="App-title">Welcome to React para los amigos de BBVA de Octubre</h1>
         </header>
         <p className="App-intro">
           Hola mundo para los amigos de BBVA de Agosto! 
